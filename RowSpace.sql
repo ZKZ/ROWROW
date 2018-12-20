09b5061efe31587d7bd0b60fc78fe55ab00d80a5
ZKZ <zhangkezhuo19881213@gmail.com>
2018-12-20 11:54:13 -0500

    Update RowSpace.sql

--- RowSpace.sql
+++ RowSpace.sql
@@ -1,1 +1,28 @@
+with
+  sample_data (country, date_id) as (
+    select 'USA', 199003 from dual union all
+    select 'USA', 200004 from dual union all
+    select 'USA', 200005 from dual union all
+    select 'USA', 200009 from dual union all
+    select 'USA', 200010 from dual union all
+    select 'UK' , 199307 from dual union all
+    select 'UK' , 199308 from dual union all
+    select 'UK' , 199408 from dual
+  ),
+TMP1 AS 
+(SELECT COUNTRY, DATE_ID, 
+TO_DATE(TO_CHAR(DATE_ID,'FM999999'),'YYYYMM')  AS DT),
+TMP2 AS 
+(SELECT COUNTRY, DATE_ID, 
+ROW_NUMBER() OVER (PARTITION BY COUNTRY ORDER BY DT) RN,
+COUNT(*) OVER (PARTITION BY COUNTRY ORDER BY DT 
+RANGE BETWEEN CURRENT ROW AND 
+)
+
+
+ FROM TMP1)
+
+
+
+
 
